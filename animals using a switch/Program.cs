0c47aa78a5a7d73d8902fb2f23620d7f3339b89b
Kristen Shea <kstar61@live.com>
2018-09-03 20:06:08 -0500

    Add project files.

---
+++ animals using a switch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace animals_using_a_switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
