d07a0544d6f74075a2a7146ddaf0b685be089af6
Kristen Shea <kstar61@live.com>
2018-09-03 20:14:30 -0500

    program to choose an animal by entering an integer using a switch statement

--- animals using a switch/Program.cs
+++ animals using a switch/Program.cs
@@ -6,7 +6,29 @@ namespace animals_using_a_switch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Pick a number: ");
+            int number = int.Parse(Console.ReadLine());
+            string animal;
+
+            switch (number)
+            {
+                case 0: animal = "Cat"; break;
+                case 1: animal = "Dog"; break;
+                case 2: animal = "Duck"; break;
+                case 3: animal = "Unicorn"; break;
+                case 4: animal = "Griffin"; break;
+                case 5: animal = "Hippogriff"; break;
+                case 6: animal = "Anteater"; break;
+                case 7: animal = "Chicken"; break;
+                case 8: animal = "Alligator"; break;
+                case 9: animal = "Cardinal"; break;
+                case 10: animal = "Sheep"; break;
+                default: animal = "No animal"; break;
+            }
+
+            Console.WriteLine($"You chose: {animal}.");
+            Console.ReadLine();
+
         }
     }
 }
